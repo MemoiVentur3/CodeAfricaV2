000e4bc83f121d6a1f568eece3303a0b0330405a
Elishaokon13 <eliscodavid@gmail.com>
2024-01-18 10:17:52 +0100

    sent

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,19 +2,19 @@ import React from "react"
 import item from '../assets/item.svg'
 import Card from "./cards"
 import ScrollingLogos from "./slider";
-
+import Partners from "./partners";
 
 export default function Team() {
 
   return (
-    
+
     <div className='my-[20px] px-[20px]'>
       <div className='container  mx-auto text-white'>
         <div className='py-4 flex flex-col justify-center items-center gap-12'>
           <div className="flex flex-col justify-center items-center">
             <div className='flex gap-2 items-center'>
               <h1 className="text-[28px] md:text-[45px] font-[700] ">Our Amazing Speakers</h1>
-              
+
             </div>
             <h4 className="w-[320px] md:text-[26px] text-[14px] text-center md:w-[700px]">Our distinguished speakers are selected from across the world and Africa to bring a wealth of knowledge and experience to Code Africa Conference.</h4>
           </div>
@@ -23,21 +23,23 @@ export default function Team() {
           </div>
         </div>
       </div>
-      <div className='mx-auto text-white'>
+      {/* <div className='mx-auto text-white'>
         <div className='py-4 flex flex-col justify-center items-center gap-12'>
           <div className="flex flex-col justify-center items-center">
             <div className='flex gap-2 items-center'>
               <h1 className="text-[28px] md:text-[45px] font-[700] ">Our Amazing Sponsors</h1>
-              
             </div>
             <h4 className="w-[320px] md:text-[26px] text-[14px] text-center md:w-[700px]">Our distinguished sponsors are selected from across the world and Africa to bring a wealth of knowledge and experience to Code Africa Conference.</h4>
+            <div className="md:hidden">
+              <Partners />
+            </div>
+            <div>
+              <ScrollingLogos className="hidden" />
+            </div>
           </div>
-          <div>
-            <ScrollingLogos />
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
-  )
+      )
 }
-
